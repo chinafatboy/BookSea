49e35fa614083443f1d4eeb73af9991921312dc7
FATBOY <fatboy88888888@hotmail.com>
2017-05-08 11:19:51 +0800

    BookSea

---
+++ ASP.NET/src/ASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP.NET.Models;
+
+namespace ASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            
+            return View();
+        }
+
+        public IActionResult book1()
+        {
+            return View();
+        }
+
+        public IActionResult register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult register(account a)
+        {
+            if (ModelState.IsValid)
+                return View();
+            else
+                return View();
+
+
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
